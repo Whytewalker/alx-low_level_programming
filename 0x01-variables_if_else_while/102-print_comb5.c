77fb94eb026901bec0f62c977383f532aaf8e425
Whytewalker <whytewalkerinstallation@gmail.com>
2023-09-09 02:44:51 +0100

    variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int i, j, k, l;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+for (k = i; k <= 57; k++)
+{
+for (l = j + 1; l <= 57; l++)
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(k);
+putchar(l);
+
+if (i == 57 && j == 56 && k == 57 && l == 57)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
