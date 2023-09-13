1ff7084e0b0c1547b5ca121835bf32037e75f6f1
Whytewalker <whytewalkerinstallation@gmail.com>
2023-09-13 23:14:55 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line.
  * @n: The number to begin counting at.
- * 
+ *
  * Return: Always 0.
  */
 void print_to_98(int n)
