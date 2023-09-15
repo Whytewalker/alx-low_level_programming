4c3149ad076952bc12b5c03449489da3a6e3ade9
Whytewalker <whytewalkerinstallation@gmail.com>
2023-09-15 03:12:15 +0100

    updated the code

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,41 +1,25 @@
 #include <stdio.h>
-#include "math.h"
 
 /**
  * prime_factor - prints the largest prime factor of a number
  * @n: The number to be checked
  * Return: Always 0.
  */
-void prime_factor(long int n)
-{
-long int i, max;
-
-max = -1;
-while (n % 2 == 0)
-{
-max = 2;
-n /= 2;
-}
-for (i = 3; i <= sqrt(n); i += 2)
-{
-while (n % i == 0)
-{
-max = i;
-n /= i;
-}
-}
-if (n > 2)
-max = n;
-printf("%ld\n", max);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-prime_factor(1231952);
+long n = 612852475143;
+long i = 2;
+long max = 0;
+
+while (n != 1)
+{
+if (n % i == 0)
+{
+n = n / i;
+max = i;
+}
+i = i + 1;
+}
+printf("%ld\n", max);
 return (0);
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_more_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * more_numbers - prints the numbers, from 0 to 9, followed by a new line
  *
  * Return: Always 0.
  */
-void print_more_numbers(void)
+void more_numbers(void)
 {
 int i, j;
 
-for (j = 0; j < 10; j++)
+for (i = 0; i < 10; i++)
 {
-for (i = 0; i < 15; i++)
+for (j = 0; j < 15; j++)
 {
-if (i >= 10)
+if (j >= 10)
 {
-_putchar(i / 10 + '0');
+_putchar(j / 10 + '0');
 }
-_putchar(i % 10 + '0');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
