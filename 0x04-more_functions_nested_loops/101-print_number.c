4c887c9d654cc376d9f08b1654b92d190736b888
Whytewalker <whytewalkerinstallation@gmail.com>
2023-09-15 19:28:48 +0100

    Update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,19 +8,17 @@
  */
 void print_number(int n)
 {
-if (n == 0)
-{
-_putchar('0');
-}
-else if (n < 0)
+unsigned int num = n;
+
+if (n < 0)
 {
 _putchar('-');
-n = -n;
+num = -num;
 }
-if (n / 10 != 0)
+if ((num / 10) > 0)
 {
-print_number(n / 10);
+print_number(num / 10);
 }
 
-_putchar((n % 10) + '0');
+_putchar((num % 10) + 48);
 }
